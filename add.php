90276040bb1d625e4e18b428eeca10132c56a0cc
Jed Boyle <jed-boyle@Jeds-iMac.local>
2021-01-30 15:39:22 +0000

    Add XXS attack protection, filters, and validation to form.

--- add.php
+++ add.php
@@ -1,10 +1,40 @@
 <?php 
 
 	if(isset($_POST['submit'])){
-		echo $_POST['email'] . '<br />';
-		echo $_POST['title'] . '<br />';
-		echo $_POST['ingredients'] . '<br />';
-	}
+
+		$email = $title = $ingredients = '';
+		
+		// check email
+		if(empty($_POST['email'])){
+			echo 'An email is required <br />';
+		} else{
+			$email = $_POST['email'];
+			if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+				echo 'Email must be a valid email address';
+			}
+		}
+
+		// check title
+		if(empty($_POST['title'])){
+			echo 'A title is required <br />';
+		} else{
+			$title = $_POST['title'];
+			if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
+				echo 'Title must be letters and spaces only';
+			}
+		}
+
+		// check ingredients
+		if(empty($_POST['ingredients'])){
+			echo 'At least one ingredient is required <br />';
+		} else{
+			$ingredients = $_POST['ingredients'];
+			if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+				echo 'Ingredients must be a comma separated list';
+			}
+		}
+
+	} // end POST check
 
 ?>
 
