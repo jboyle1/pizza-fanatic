d62964305d9c8f81765875d4bdb4bbb41c629af0
Jed Boyle <jed-boyle@Jeds-iMac.local>
2021-01-31 11:19:37 +0000

    Connect to phpmyadmin database and write SQL query to recieve data,

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php 
 
+	// connect to the database
+	$conn = mysqli_connect('localhost', 'jed', 'test1234', 'pizza_fanatic');
+
+	// check connection
+	if(!$conn){
+		echo 'Connection error: '. mysqli_connect_error();
+	}
+
+	// write query for all pizzas
+	$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
+
+	// get the result set (set of rows)
+	$result = mysqli_query($conn, $sql);
+
+	// fetch the resulting rows as an array
+	$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+	// free the $result from memory (good practise)
+	mysqli_free_result($result);
+
+	// close connection
+	mysqli_close($conn);
+
+	print_r($pizzas);
+
 
 ?>
 
@@ -7,6 +32,39 @@
 <html>
 
 <?php include('templates/header.php'); ?>
+
+<h4 class="center grey-text">Pizzas!</h4>
+
+<div class="container">
+    <div class="row">
+
+        <?php foreach($pizzas as $pizza): ?>
+
+        <div class="col s6 md3">
+
+            <div class="card z-depth-0">
+
+                <div class="card-content center">
+
+                    <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+
+                    <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+
+                </div>
+
+                <div class="card-action right-align">
+                    <div class="brand-text" href="#">more info</div>
+                </div>
+
+            </div>
+
+        </div>
+
+        <?php endforeach; ?>
+
+
+    </div>
+</div>
 
 <?php include('templates/footer.php'); ?>
 
